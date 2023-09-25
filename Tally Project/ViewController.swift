71245261289e22b9303be0957b20eaa5e18785be
Hunter Jensen <hunter.jensen.cbc@gmail.com>
2023-09-25 08:14:37 -0600

    Initial Commit

---
+++ Tally Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tally Project
+//
+//  Created by Hunter Jensen on 9/25/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
